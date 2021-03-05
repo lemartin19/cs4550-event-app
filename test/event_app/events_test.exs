bd2fd0dcfbeca144ea6adda14073ab83b3a6e06e
lemartin19 <lmartin411@gmail.com>
2021-03-05 21:50:57 +0000

    preload user in unit tests

--- test/event_app/events_test.exs
+++ test/event_app/events_test.exs
@@ -1,12 +1,13 @@
 defmodule EventApp.EventsTest do
   use EventApp.DataCase
 
+  alias EventApp.Repo
   alias EventApp.Events
 
   describe "events" do
     alias EventApp.Events.Event
 
-    @valid_attrs %{date: ~N[2010-04-17 14:00:00], description: "some description", name: "some name"}
+    @valid_attrs %{date: ~N[2010-04-17 14:00:00], description: "some description", name: "some name", user_id: nil}
     @update_attrs %{date: ~N[2011-05-18 15:01:01], description: "some updated description", name: "some updated name"}
     @invalid_attrs %{date: nil, description: nil, name: nil}
 
@@ -21,11 +22,13 @@ defmodule EventApp.EventsTest do
 
     test "list_events/0 returns all events" do
       event = event_fixture()
+      |> Repo.preload(:user)
       assert Events.list_events() == [event]
     end
 
     test "get_event!/1 returns the event with given id" do
       event = event_fixture()
+      |> Repo.preload(:user)
       assert Events.get_event!(event.id) == event
     end
 
@@ -42,6 +45,7 @@ defmodule EventApp.EventsTest do
 
     test "update_event/2 with valid data updates the event" do
       event = event_fixture()
+      |> Repo.preload(:user)
       assert {:ok, %Event{} = event} = Events.update_event(event, @update_attrs)
       assert event.date == ~N[2011-05-18 15:01:01]
       assert event.description == "some updated description"
@@ -50,6 +54,7 @@ defmodule EventApp.EventsTest do
 
     test "update_event/2 with invalid data returns error changeset" do
       event = event_fixture()
+      |> Repo.preload(:user)
       assert {:error, %Ecto.Changeset{}} = Events.update_event(event, @invalid_attrs)
       assert event == Events.get_event!(event.id)
     end
