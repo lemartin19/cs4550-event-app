943f6bacf89c8e2f1966145d66b8ba95949834e9
lemartin19 <lmartin411@gmail.com>
2021-03-08 23:28:37 -0500

    finish allowing a viewable profile for user and delete user

--- lib/event_app_web/controllers/user_controller.ex
+++ lib/event_app_web/controllers/user_controller.ex
@@ -3,6 +3,34 @@ defmodule EventAppWeb.UserController do
 
   alias EventApp.Users
   alias EventApp.Users.User
+  alias EventAppWeb.SessionController
+
+  alias EventAppWeb.Plugs
+  plug Plugs.RequireUser when action not in [
+    :new, :create, :show]
+  plug :fetch_user when action in [
+    :show, :edit, :update, :delete]
+  plug :require_owner when action in [
+    :edit, :update, :delete]
+
+  def fetch_user(conn, _args) do
+    id = conn.params["id"]
+    user = Users.get_user!(id)
+    assign(conn, :user, user)
+  end
+
+  def require_owner(conn, _args) do
+    current_user = conn.assigns[:current_user]
+    user = conn.assigns[:user]
+    if current_user.id == user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You do not own this.")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
+    end
+  end
 
   def new(conn, _params) do
     changeset = Users.change_user(%User{})
@@ -24,18 +52,18 @@ defmodule EventAppWeb.UserController do
   end
 
   def show(conn, %{"id" => id}) do
-    user = Users.get_user!(id)
+    user = conn.assigns[:user]
     render(conn, "show.html", user: user)
   end
 
   def edit(conn, %{"id" => id}) do
-    user = Users.get_user!(id)
+    user = conn.assigns[:user]
     changeset = Users.change_user(user)
     render(conn, "edit.html", user: user, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Users.get_user!(id)
+    user = conn.assigns[:user]
 
     case Users.update_user(user, user_params) do
       {:ok, user} ->
@@ -49,11 +77,11 @@ defmodule EventAppWeb.UserController do
   end
 
   def delete(conn, %{"id" => id}) do
-    user = Users.get_user!(id)
+    user = conn.assigns[:user]
     {:ok, _user} = Users.delete_user(user)
 
     conn
     |> put_flash(:info, "User deleted successfully.")
-    |> redirect(to: Routes.user_path(conn, :index))
+    |> SessionController.delete(%{})
   end
 end
